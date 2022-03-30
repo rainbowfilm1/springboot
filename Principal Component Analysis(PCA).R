b1d3a4ac46b20c8786490833da3c49a8e76398d2
rainbowfilm1 <102548628+rainbowfilm1@users.noreply.github.com>
2022-03-31 00:28:46 +0530

    Add files via upload

---
+++ Principal Component Analysis(PCA).R
@@ -0,0 +1,27 @@
+#Practical of Principal Component Analysis(PCA)
+
+
+iris
+data_iris<-iris[1:4]
+data_iris
+Cov_data<-cov(data_iris)
+Cov_data
+
+Eigen_data<-eigen(Cov_data)
+Eigen_data
+
+PCA_data<-princomp(data_iris,cor="False")
+PCA_data
+
+Eigen_data$values
+PCA_data$sdev^2
+PCA_data$loadings[,1:4]
+Eigen_data$vectors
+summary(PCA_data)
+biplot(PCA_data)
+
+screeplot(PCA_data,type = "lines")
+model2_scores<-as.matrix(data_iris)%*%model2
+library(e1071)
+mod1<-naiveBayes(iris[,1:4],iris[,5])
+table(predict(mod1,iris[,1:4]),iris[,5])
